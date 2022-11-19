d52d5683568a0077392c41377bccec3f7f95992d
Charles Brendon <charlesbrendonss@gmail.com>
2022-11-18 21:36:25 -0300

    feat create dto admin & entity

--- src/admin/dto/create-admin.dto.ts
+++ src/admin/dto/create-admin.dto.ts
@@ -1,1 +1,28 @@
-export class CreateAdminDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsNotEmpty, IsEmail, Matches } from "class-validator";
+
+export class CreateAdminDto {
+  @ApiProperty({
+    description: "Admin`s name",
+    example: "virtumed",
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    description: "Admin`s login",
+    example: "virtumeds2@equipe3.com",
+  })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    description: "Admin password",
+    example: "virtumed@123",
+  })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: "Too weak",
+  })
+  password: string;
+}
