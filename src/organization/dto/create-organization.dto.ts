c30256790c87f942c52c38f68c49c71200aa219a
Charles Brendon <charlesbrendonss@gmail.com>
2022-11-21 00:01:16 -0300

    create: crud organization

--- src/organization/dto/create-organization.dto.ts
+++ src/organization/dto/create-organization.dto.ts
@@ -2,21 +2,24 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString, Matches } from "class-validator";
 
 export class CreateOrganizationDto {
+  // NAME
   @ApiProperty({
     description: "Nome da Organization (Hospital / Clínica)",
-    example: "Hospital das Clínicas"
+    example: "Hospital das Clínicas",
   })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  // LOGIN
   @ApiProperty({
     description: "Organization / Hospital / Clinic email",
-    example: "email@organization.com"
+    example: "email@organization.com",
   })
   @IsEmail()
   email: string;
 
+  // PASSWORD
   @ApiProperty({
     description: "Admin password",
     example: "@Abcd1234",
@@ -26,23 +29,27 @@ export class CreateOrganizationDto {
   })
   password: string;
 
+  // CNPJ
   @ApiProperty({
     description: "Número do CNPJ",
-    example: "10.000.000/0001-01"
+    example: "10.000.000/0001-01",
   })
   @IsString()
   cnpj: string;
-
+  
+  // IMAGE
   @ApiProperty({
     description: "Imagem ou URL da imagem",
-    example: "https://crbm5.gov.br/novosite/wp-content/uploads/2021/04/HCPA-site-1024x569.png"
+    example:
+      "https://crbm5.gov.br/novosite/wp-content/uploads/2021/04/HCPA-site-1024x569.png",
   })
   @IsString()
   image: string;
 
+  // ROLE
   @ApiProperty({
     description: "Função no sistema",
-    example: "organization"
+    example: "organization",
   })
   @IsString()
   role: string;
--- src/organization/organization.service.ts
+++ src/organization/organization.service.ts
@@ -1,45 +1,46 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateOrganizationDto } from './dto/create-organization.dto';
-import { UpdateOrganizationDto } from './dto/update-organization.dto';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { CreateOrganizationDto } from "./dto/create-organization.dto";
+import { UpdateOrganizationDto } from "./dto/update-organization.dto";
 
 @Injectable()
 export class OrganizationService {
   constructor(private prisma: PrismaService) {}
 
   async findOne(id: string) {
-    const record = this.prisma.organization.findUnique({ where: {id}})
-    if(!record){
-     throw new NotFoundException(
-      `registro da organização com o ID: ${id} não encontrado`
-     )
+    const record = this.prisma.organization.findUnique({ where: { id } });
+    if (!record) {
+      throw new NotFoundException(
+        `registro da organização com o ID: ${id} não encontrado`
+      );
     }
     return record;
   }
-  
+
   findAll() {
     return this.prisma.organization.findMany();
   }
 
   async create(createOrganizationDto: CreateOrganizationDto) {
     const organization: any = { ...CreateOrganizationDto };
-    return this.prisma.organization.create({ data: organization}).catch(this.handleError);
+    return this.prisma.organization
+      .create({ data: organization })
+      .catch(this.handleError);
   }
   handleError(error: Error) {
     console.log(error);
     return undefined;
   }
 
-
   async update(id: string, updateOrganizationDto: UpdateOrganizationDto) {
-    await this.findOne(id)
+    await this.findOne(id);
     const data: any = { ...updateOrganizationDto };
-    return this.prisma.organization.update({ data, where: { id }});
+    return this.prisma.organization.update({ data, where: { id } });
   }
 
   async remove(id: string) {
-    await this.findOne(id)
-    await this.prisma.organization.delete({ where: { id }})
-    return `Registro da Organização com o ID:${id} deletado com sucesso`;;
+    await this.findOne(id);
+    await this.prisma.organization.delete({ where: { id } });
+    return `Registro da Organização com o ID:${id} deletado com sucesso`;
   }
 }
