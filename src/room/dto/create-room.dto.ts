e9a4f1126fe91fd257da1b03c6ec7c7d69ca93b3
Charles Brendon <charlesbrendonss@gmail.com>
2022-12-02 20:58:54 -0300

    Create crud room

---
+++ src/room/dto/create-room.dto.ts
@@ -0,0 +1,3 @@
+export class CreateRoomDto {
+    id?: string
+}
---
+++ src/room/room.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { RoomService } from './room.service';
+import { CreateRoomDto } from './dto/create-room.dto';
+import { UpdateRoomDto } from './dto/update-room.dto';
+
+@Controller('room')
+export class RoomController {
+  constructor(private readonly roomService: RoomService) {}
+
+  @Post()
+  create(@Body() createRoomDto: CreateRoomDto) {
+    return this.roomService.create(createRoomDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.roomService.findOne(id);  }
+
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.roomService.remove(id);
+  }
+}
