3005c65bbb2b64f7541154913805861bcecdf238
Charles Brendon <charlesbrendonss@gmail.com>
2023-01-25 19:25:03 -0300

    fix(password): organizations

--- src/organization/dto/create-organization.dto.ts
+++ src/organization/dto/create-organization.dto.ts
@@ -31,6 +31,7 @@ export class CreateOrganizationDto {
     example: "+5511912345678",
   })
   // @IsMobilePhone('pt-BR')
+  @Matches(/^((\+?55\ ?[1-9]{2}\ ?)|(0[1-9]{2}\ ?)|(\([1-9]{2}\)\ ?)|([1-9]{2}\ ?))((\d{4}\-?\d{4})|(9[6-9]{1}\d{3}\-?\d{4}))$/)
   phone: string;
 
   // PASSWORD
