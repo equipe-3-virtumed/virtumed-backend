281f64906d4c094a7979545e520b5a0ce97a3fbc
Bruno Sallum <brusallum@hotmail.com>
2023-02-02 17:54:38 -0300

    chore(sockets): work in progress

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -20,13 +20,23 @@ export class ChatGateway implements OnGatewayInit {
   @SubscribeMessage('joinRoom')
   handleRoomJoin(client: Socket, room: string) {
     client.join(room);
-    // console.log("🚀 ~ file: chat.gateway.ts:23 ~ ChatGateway ~ handleRoomJoin ~ room", room)
     client.emit('joinedRoom', client.id);
   }
 
   @SubscribeMessage('ready')
-  handleReady(client: Socket, credentials: { roomId: string; localParticipant: string }) {
-    this.wss.to(credentials.roomId).emit('readyToGo', credentials.localParticipant);
+  handleReady(
+    client: Socket,
+    data: { roomId: string; localParticipant: string },
+  ) {
+    this.wss.to(data.roomId).emit('readyToGo');
+  }
+
+  @SubscribeMessage('idExchange')
+  handleIds(
+    client: Socket,
+    room: string
+  ) {
+    this.wss.to(room).emit('idExchange', client.id)
   }
 
   @SubscribeMessage('chatToServer')
@@ -40,11 +50,11 @@ export class ChatGateway implements OnGatewayInit {
   }
 
   @SubscribeMessage('videoRequest')
-  handleStream(client: Socket, data: { room: string; signal: any; }) {
-    // console.log("🚀 ~ file: chat.gateway.ts:44 ~ ChatGateway ~ handleStream ~ data", data);
+  handleRequest(client: Socket, data: { room: string; signal: any }) {
+    console.log("🚀 ~ file: chat.gateway.ts:46 ~ ChatGateway ~ handleStream ~ data", data)
     const { signal } = data;
     const { id } = client;
-    const stream = { signal, id}
-    this.wss.to(data.room).emit('videoStream', stream);
+    const stream = { signal, id };
+    this.wss.to(data.room).emit('videoRequest', stream);
   }
 }
